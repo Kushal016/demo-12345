07a9b1d0701700b9bda1e15f9f87cba39e9e16ff
Kushal016 <67750474+Kushal016@users.noreply.github.com>
2021-04-23 17:54:42 +0530

    Update config.php
    connected with remote server

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ ob_start();//turns on output buffering
 session_start();
 
 $timezone = date_default_timezone_set("Asia/Kolkata");
-$con = mysqli_connect('remotemysql.com','rYchKMd5zM','QxQdOliAuk','rYchKMd5zM');
+$con = mysqli_connect('remotemysql.com','joSBOdvfIo','KqaHKmBnyW','joSBOdvfIo');
 if(mysqli_connect_errno()){
 	echo "Failed".mysqli_connect_errno(); 
 }
